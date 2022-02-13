baaf81b244f4e8529c1b0a67028119f5bdc10a20
NS-DILLON-D <dillondow24@gmail.com>
2022-02-13 18:54:47 -0500

    footer added

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,10 +2,11 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { Box, Container, CssBaseline } from '@mui/material';
+import { Box, Container, CssBaseline, useTheme } from '@mui/material';
 import React from 'react';
 import { Route, Routes } from "react-router-dom";
 import './App.css';
+import Footer from './components/Footer';
 import Header from './components/Header';
 import Games from './routes/Games';
 import Login from './routes/Login';
@@ -14,22 +15,25 @@ import SignUp from './routes/SignUp';
 import Teams from './routes/Teams';
 
 function App() {
+  const theme = useTheme();
+  
   return (
     <React.Fragment>
       <CssBaseline />
       <Header/>
-      <Container fixed >
-        <Routes>
-          <Route path="/" element={<Games />} />
-          <Route path="Games" element={<Games />} />
-          <Route path="Teams" element={<Teams />} />
-          <Route path="Profile" element={<Profile />} />
-          <Route path="Login" element={<Login />} />
-          <Route path="SignUp" element={<SignUp />} />
-        </Routes>
-        <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }}>
+      <Container maxWidth='lg'>
+        <Box sx={{ mt: theme.spacing(1), bgcolor: '#cfe8fc', minHeight: '100vh' }}>
+          <Routes>
+            <Route path="/" element={<Games />} />
+            <Route path="Games" element={<Games />} />
+            <Route path="Teams" element={<Teams />} />
+            <Route path="Profile" element={<Profile />} />
+            <Route path="Login" element={<Login />} />
+            <Route path="SignUp" element={<SignUp />} />
+          </Routes>
         </Box>
       </Container>
+      <Footer />
     </React.Fragment>
   );
 }
